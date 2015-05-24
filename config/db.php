f597cc5bc2cd8c8fc1d82d5e354978c87de47b10
Kevin Leng <ldh2068vip@gmail.com>
2015-05-24 11:32:59 +0800

    add db config

--- config/db.php
+++ config/db.php
@@ -1,9 +1,9 @@
 <?php
-
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii',
-    'username' => 'root',
-    'password' => '',
-    'charset' => 'utf8',
+return [ 
+		'class' => 'yii\db\Connection',
+		'dsn' => 'mysql:host=localhost;dbname=520yl',
+		'username' => 'root',
+		'password' => '',
+		'charset' => 'utf8',
+		'tablePrefix' => 'yl_' 
 ];
